0e4ced33c8007dd3bd26bbc314dfaa03080e82c9
Daniel Prokhorov <d.prokhorov@sparks-gmbh.de>
2021-12-15 12:05:12 +0100

    Removed jetbrains annotations.

--- ProjectExplorerTree/ObservableObject.cs
+++ ProjectExplorerTree/ObservableObject.cs
@@ -6,8 +6,7 @@ namespace ProjectExplorerTree;
 public class ObservableObject : INotifyPropertyChanged
 {
     public event PropertyChangedEventHandler? PropertyChanged;
-
-    [NotifyPropertyChangedInvocator]
+    
     protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
     {
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
--- ProjectExplorerTree/TreeNodeTypes/SimpleFileTreeNode.cs
+++ ProjectExplorerTree/TreeNodeTypes/SimpleFileTreeNode.cs
@@ -1,6 +1,5 @@
 ﻿namespace ProjectExplorerTree.TreeNodeTypes;
 
-[UsedImplicitly]
 public sealed class SimpleFileTreeNode : FileTreeNode
 {
     public SimpleFileTreeNode(string name, TreeNodeBase? parent) : base(parent)
